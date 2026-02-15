9dc3db9e00eeef09ccd5b45bace1c1bab3fadf1f
Dipesh Budhathoki <budhatdh@mail.uc.edu>
2026-02-15 17:21:50 -0500

    Complete dashboard Project

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,28 +1,32 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from sqlite3 import connect
+from pathlib import Path
+import pandas as pd
+from .sql_execution import QueryMixin  # Import the mixin for query execution
 
+# Define the database path
+db_path = Path(__file__).parent / "employee_events.db"
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
+class QueryBase(QueryMixin):
 
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+    name = ""
 
     # Define a `names` method that receives
     # no passed arguments
-    # YOUR CODE HERE
+    def names(self):
         
         # Return an empty list
-        # YOUR CODE HERE
+        return []
 
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
-
+    def event_counts(self, entity_id):
         # QUERY 1
         # Write an SQL query that groups by `event_date`
         # and sums the number of positive and negative events
@@ -31,13 +35,22 @@
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        # YOUR CODE HERE
+        sql = f"""
+            SELECT event_date, 
+                   SUM(positive_events) AS total_positive_events, 
+                   SUM(negative_events) AS total_negative_events
+            FROM employee_events
+            WHERE {self.name}_id = ?
+            GROUP BY event_date
+            ORDER BY event_date;
+            """
+        return self.pandas_query(sql, params = (entity_id,))
             
     
 
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
-    # YOUR CODE HERE
+    def notes(self, entity_id):
 
         # QUERY 2
         # Write an SQL query that returns `note_date`, and `note`
@@ -46,5 +59,11 @@
         # with f-string formatting
         # so the query returns the notes
         # for the table name in the `name` class attribute
-        # YOUR CODE HERE
+        query = f"""
+            SELECT note_date, note
+            FROM notes
+            where {self.name}_id = ?
+            ORDER BY note_date;
+            """
+        return self.pandas_query(query, params = (entity_id,))
 
--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,45 +1,52 @@
 # Import the QueryBase class
-# YOUR CODE HERE
-
+from .query_base import QueryBase
 # Import dependencies for sql execution
-#### YOUR CODE HERE
+from .sql_execution import QueryMixin
 
 # Create a subclass of QueryBase
 # called  `Team`
-#### YOUR CODE HERE
+class Team(QueryBase):
 
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
+    name = "team"
 
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    def names(self)->list[tuple]:
         
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
-        #### YOUR CODE HERE
+        sql = """
+            SELECT team_name, team_id
+            FROM team;
+            """
+        return self.query(sql)
     
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-
+    def username(self, entity_id)->list[tuple]:
         # Query 6
         # Write an SQL query
         # that selects the team_name column
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-        #### YOUR CODE HERE
+        sql = f"""
+            SELECT team_name
+            FROM team
+            WHERE team_id = ?;
+            """
+        return self.query(sql,[entity_id])
 
 
     # Below is method with an SQL query
@@ -50,9 +57,8 @@
     # is returns containing the execution of
     # the sql query
     #### YOUR CODE HERE
-    def model_data(self, id):
-
-        return f"""
+    def model_data(self, entity_id):
+        sql =  f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
@@ -60,7 +66,8 @@
                     FROM {self.name}
                     JOIN employee_events
                         USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
+                    WHERE {self.name}.{self.name}_id = ?
                     GROUP BY employee_id
                    )
                 """
+        return self.pandas_query(sql, params = (entity_id,))
